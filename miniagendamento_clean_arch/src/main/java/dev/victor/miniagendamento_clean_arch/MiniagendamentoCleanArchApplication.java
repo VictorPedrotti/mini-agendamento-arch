471c2f45d42f7fda773f28451d725193cc5bfc91
Victor Pedrotti <victor1404pedrotti@gmail.com>
2025-11-27 09:54:47 -0300

    feat: criando esquema de pastas da arquitetura

---
+++ miniagendamento_clean_arch/src/main/java/dev/victor/miniagendamento_clean_arch/MiniagendamentoCleanArchApplication.java
@@ -0,0 +1,13 @@
+package dev.victor.miniagendamento_clean_arch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniagendamentoCleanArchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniagendamentoCleanArchApplication.class, args);
+	}
+
+}
---
+++ miniagendamento_clean_arch/src/test/java/dev/victor/miniagendamento_clean_arch/MiniagendamentoCleanArchApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.victor.miniagendamento_clean_arch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniagendamentoCleanArchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
